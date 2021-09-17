e2417a58f7934f876d158009f8fb342d820a0695
Alan Sanchez <asanchez@hello-robot.com>
2021-09-17 10:13:17 -0700

    Updated multipoint command node. Needed to send trajectory goal to client.

--- src/multipoint_command.py
+++ src/multipoint_command.py
@@ -13,37 +13,31 @@ class MultiPointCommand(hm.HelloNode):
 
     def issue_multipoint_command(self):
       point0 = JointTrajectoryPoint()
-      point0.time_from_start = rospy.Duration(0.000)
       point0.positions = [0.2, 0.0, 3.4]
       point0.velocities = [0.2]
       point0.accelerations = [1.0]
 
       point1 = JointTrajectoryPoint()
-      point1.time_from_start = rospy.Duration(0.5)
       point1.positions = [0.3, 0.1, 2.0]
       point1.velocities = [0.1]
       point1.accelerations = [-1.0]
 
       point2 = JointTrajectoryPoint()
-      point2.time_from_start = rospy.Duration(1.0)
-      point2.positions = [0.5, 0.2, 1.0]
-      point2.velocities = [0.02]
+      point2.positions = [0.5, 0.2, -1.0]
+      point2.velocities = [0.08]
       point2.accelerations = [-1.0]
 
       point3 = JointTrajectoryPoint()
-      point3.time_from_start = rospy.Duration(1.5)
       point3.positions = [0.6, 0.3, 0.0]
       point3.velocities = [0.1]
       point3.accelerations = [1.0]
 
       point4 = JointTrajectoryPoint()
-      point4.time_from_start = rospy.Duration(2.0)
-      point4.positions = [0.8, 0.2, -1.0]
+      point4.positions = [0.8, 0.2, 1.0]
       point4.velocities = [0.2]
       point4.accelerations = [1]
 
       point5 = JointTrajectoryPoint()
-      point5.time_from_start = rospy.Duration(2.5)
       point5.positions = [0.5, 0.1, 0.0]
 
 
@@ -53,16 +47,21 @@ class MultiPointCommand(hm.HelloNode):
       trajectory_goal.trajectory.header.stamp = rospy.Time(0.0)
       trajectory_goal.trajectory.header.frame_id = 'base_link'
 
+      self.trajectory_client.send_goal(trajectory_goal)
+      rospy.loginfo('Sent stow goal = {0}'.format(trajectory_goal))
+      self.trajectory_client.wait_for_result()
+
+
     def main(self):
         hm.HelloNode.main(self, 'multipoint_command', 'multipoint_command', wait_for_first_pointcloud=False)
         rospy.loginfo('issuing multipoint command...')
-        self.issue_multipoint_multijoint_command()
+        self.issue_multipoint__command()
         time.sleep(2)
 
 
 if __name__ == '__main__':
     try:
-        node = StowCommand()
+        node = MultiPointCommand()
         node.main()
     except KeyboardInterrupt:
         rospy.loginfo('interrupt received, so shutting down')
