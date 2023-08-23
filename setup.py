06584468e73210f884a15906ab18e355d7d70175
hello-jesus <141784078+hello-jesus@users.noreply.github.com>
2023-08-23 15:14:10 -0700

    Update de setup.py, add the rviz pkg name for the perception tutorial for ROS2

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'stretch_ros_tutorials'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/rviz', glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
